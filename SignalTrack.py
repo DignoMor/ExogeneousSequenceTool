b9efe2ef565d0959b03f04e134e4583f240af1fb
Xiuqi Pan <xp76@cornell.edu>
2025-06-09 15:34:19 -0400

    Add range option for argmax and corresponding tests

--- SignalTrack.py
+++ SignalTrack.py
@@ -1,4 +1,6 @@
 
+import numpy as np
+
 from RGTools.ExogeneousSequences import ExogeneousSequences
 
 class SignalTrack:
@@ -15,12 +17,26 @@ class SignalTrack:
                             help="Path to the output stat npy file.",
                             required=True,
                             )
+        
+        parser.add_argument("--search_range",
+                            help="Search range for the signal track. Format: 'start,end'. "
+                                 "If not provided, the whole signal track will be used. "
+                                 "Range follows half-open and 0-index convention.",
+                            default=None, 
+                            type=str,
+                            )
 
     @staticmethod
     def argmax_main(args):
         es = ExogeneousSequences(args.fasta)
         es.load_region_anno_from_npy("signal_track", args.input_npy)
         signal_track = es.get_anno_arr("signal_track")
+
+        if args.search_range: 
+            start, end = map(int, args.search_range.split(","))
+            signal_track[:, :start] = -np.inf
+            signal_track[:, end:] = -np.inf
+
         output_stat = signal_track.argmax(axis=1, keepdims=True)
         es.load_region_anno_from_arr("stat", output_stat)
         es.save_anno_npy("stat", args.output_npy)
--- tests/SignalTrackTest.py
+++ tests/SignalTrackTest.py
@@ -25,6 +25,7 @@ class SignalTrackTest(unittest.TestCase):
             fasta=os.path.join(self.test_dir, "fasta.fasta"),
             input_npy=os.path.join(self.test_dir, "input.npy"),
             output_npy=os.path.join(self.test_dir, "output.npy"),
+            search_range=None,
         )
         
         ExogeneousSequences.write_sequences_to_fasta(
@@ -46,5 +47,38 @@ class SignalTrackTest(unittest.TestCase):
         self.assertEqual(output_track[0, 0], 3)
         self.assertEqual(output_track[1, 0], 8)
         self.assertEqual(output_track[2, 0], 4)
+
+    def test_argmax_with_search_range(self):
+        args = argparse.Namespace(
+            fasta=os.path.join(self.test_dir, "fasta.fasta"),
+            input_npy=os.path.join(self.test_dir, "input.npy"),
+            output_npy=os.path.join(self.test_dir, "output.npy"),
+            search_range="2,6",
+        )
+        
+        ExogeneousSequences.write_sequences_to_fasta(
+            ["seq1", "seq2", "seq3"],
+            ["ATCG", "TTGA", "CCAT"],
+            args.fasta,
+        )
+
+        tracks = np.zeros((3, 10))
+        tracks[0, 3:7] = 1
+        tracks[0, 5] = 2
+        tracks[0, 7] = 3
+        tracks[1, 4] = 1
+        tracks[2, 4] = 1
+        tracks[2, 5] = 2
+        np.save(args.input_npy, tracks)
+        
+        SignalTrack.argmax_main(args)
+
+        output_track = np.load(args.output_npy)
+        self.assertEqual(output_track.shape, (3, 1))
+        self.assertEqual(output_track[0, 0], 5)
+        self.assertEqual(output_track[1, 0], 4)
+        self.assertEqual(output_track[2, 0], 5)
+
+
 
 
